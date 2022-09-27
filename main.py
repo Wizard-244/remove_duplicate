d44ff5962cce63c999c29921b21fbaac955ac7a4
Wizard-244 <warlock244@yahoo.com>
2022-09-27 16:29:10 +0300

    Test Ok

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+print ("Remove duplicate")
+print("Enter a character or numbers")
+s_smbl = input()
+f_smbl = ""
+for i in range(len(s_smbl)):
+    if f_smbl.find(s_smbl[i]) == -1 and s_smbl[i] != " ":
+        f_smbl += s_smbl[i]
+print("Clean data")
+print(f_smbl)
